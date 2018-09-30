e18667544c7ffcaf847babe5b3ca95c660fe4f39
Teck Onn <teckonn@wobb.co>
2018-09-30 21:19:21 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "New List",
+      home: MyHomePage(),
+    );
+  }
+}
+ 
+class MyHomePage extends StatefulWidget{
+  @override
+  MyHomePageState createState() => new MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage>{
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 20.0);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold (
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context,i){
+        if (i.isOdd) return Divider();
+        final index = i ~/ 2;
+        // The syntax "i ~/ 2" divides i by 2 and returns an integer result.
+        // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+        if (index >= _suggestions.length){
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
