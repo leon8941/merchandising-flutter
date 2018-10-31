516ba6c17eac94c4cf4d1f4421ec1b56a596467b
Leon M <leon8941@gmail.com>
2018-10-31 22:52:19 +0800

    Add google firebase

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,8 +5,9 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
+    [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+//    [FIRApp configure];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
