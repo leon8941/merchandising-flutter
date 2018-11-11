139cc9d7de991510f9eb67d32123a0f3c39ec4be
Leon M <leon8941@gmail.com>
2018-11-11 17:22:27 +0800

    Add users container in message list.

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,10 @@ import 'User.dart';
 import 'Choice.dart';
 import 'const.dart'; 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart'; 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(new MyApp());
 
@@ -71,6 +73,11 @@ class MyHomePageState extends State<MyHomePage>{
 
   @override
   Widget build(BuildContext context){
+    var snapshots = Firestore.instance.collection('users').snapshots();
+
+    print("build snapshots");
+    print(snapshots.toList());
+
     return Scaffold (
       appBar: AppBar(
         title: Text('Messages'),
@@ -102,8 +109,136 @@ class MyHomePageState extends State<MyHomePage>{
           )
         ],
       ),
-      body: _buildSuggestions(),
+      // body: _buildSuggestions(),
+      body: _userList(),
     );
+  }
+
+  Widget _userList() {
+    return WillPopScope(
+      child: Stack(
+        children: <Widget>[
+          //List of users
+          _user()
+        ],
+      ),
+      onWillPop: onBackPress,
+    );
+  }
+
+  Widget _user() {
+    return Container(
+      child: StreamBuilder(
+        stream: Firestore.instance.collection('users').snapshots(),
+        builder: (context, snapshot) {
+          if(!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(themeColor),
+              ),
+            );
+          } 
+          else {
+            print("_user() snapshot");
+            print(snapshot);
+            return _buildUser(snapshot);
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildUser(snapshot) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8.0),
+      itemCount: snapshot.data.documents.length,
+      itemBuilder: (context, i){
+        return _buildItem(context, snapshot.data.documents[i]);
+      },
+    );
+  }
+
+  Widget _buildItem(context, DocumentSnapshot document){
+    print("currentUserId");
+    print(currentUserId);
+
+    print("document['id']");
+    print(document['id']);
+
+    if(document['id'] == currentUserId) {
+      return Container();
+    }
+    else {
+      return Container(
+        child: FlatButton(
+          child: Row(
+            children: <Widget>[
+              Material(
+                child: CachedNetworkImage(
+                  placeholder: Container(
+                    child: CircularProgressIndicator(
+                      strokeWidth: 1.0,
+                      valueColor: AlwaysStoppedAnimation<Color>(themeColor),
+                    ),
+                    width: 50.0,
+                    height: 50.0,
+                    padding: EdgeInsets.all(15.0),
+                  ),
+                  imageUrl: document['photoUrl'],
+                  width: 50.0,
+                  height: 50.0,
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                clipBehavior: Clip.hardEdge,
+              ),
+              new Flexible(
+                child: Container(
+                  child: new Column(
+                    children: <Widget>[
+                      new Container(
+                        child: Text(
+                          'Nickname: ${document['nickname']}',
+                          style: TextStyle(color: primaryColor),
+                        ),
+                        alignment: Alignment.centerLeft,
+                        margin: new EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 5.0),
+                      ),
+                      new Container(
+                        child: Text(
+                          'About me: ${document['aboutMe'] ?? 'Not available'}',
+                          style: TextStyle(color: primaryColor),
+                        ),
+                        alignment: Alignment.centerLeft,
+                        margin: new EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                      )
+                    ],
+                  ),
+                  margin: EdgeInsets.only(left: 20.0),
+                ),
+              ),
+            ],
+          ),
+          onPressed: () {
+            Fluttertoast.showToast(msg: "Sign in success");
+          },
+          // onPressed: () { // To Message Detail
+          //   Navigator.push(
+          //       context,
+          //       new MaterialPageRoute(
+          //           builder: (context) => new Chat(
+          //                 peerId: document.documentID,
+          //                 peerAvatar: document['photoUrl'],
+          //               )));
+          // },
+          color: greyColor2,
+          padding: EdgeInsets.fromLTRB(25.0, 10.0, 25.0, 10.0),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        ),
+        margin: EdgeInsets.only(bottom: 10.0, left: 5.0, right: 5.0),
+      );
+    }
   }
 
   Widget _buildSuggestions(){
